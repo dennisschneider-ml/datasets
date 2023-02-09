fa11427c5f5aa5c680af83ee1752881b2b6d6dc0
Dennis Schneider <d.schneider.ulm@gmx.de>
2023-02-09 21:27:03 +0100

    IMPR: Removed redundant lines

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-echo "Being executed to $WORKDIR ..."
-[ -f $WORKDIR/Makefile ] || echo "Makefile being created ..."
 [ -f $WORKDIR/Makefile ] || cp ./assets/Makefile $WORKDIR
-[ -d $WORKDIR/data/ ] || mkdir $WORKDIR/data/
-
